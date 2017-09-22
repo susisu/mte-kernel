0bef083c93ae98f9fef2236c4c0d49e403610146
Susisu <susisu2413@yahoo.co.jp>
2017-09-23 00:03:14 +0900

    Add `_readRow` function

--- test/parser.js
+++ test/parser.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 
-import { _splitCells } from "../lib/parser.js";
+import { TableRow } from "../lib/table-row.js";
+import { _splitCells, _readRow } from "../lib/parser.js";
 
 /**
  * @test {_splitCells}
@@ -22,5 +23,71 @@ describe("_splitCells(text)", () => {
     expect(_splitCells(" | `\\` | B | ")).to.deep.equal([" ", " `\\` ", " B ", " "]);
     expect(_splitCells(" | A `\\|` B | ")).to.deep.equal([" ", " A `\\|` B ", " "]);
     expect(_splitCells(" | A \\`|` B | ")).to.deep.equal([" ", " A \\`", "` B ", " "]);
+  });
+});
+
+/**
+ * @test {_readRow}
+ */
+describe("_readRow(text)", () => {
+  it("should read a table row", () => {
+    {
+      const row = _readRow("");
+      expect(row).to.be.an.instanceOf(TableRow);
+      expect(row.marginLeft).to.equal("");
+      expect(row.marginRight).to.equal("");
+      const cells = row.getCells();
+      expect(cells).to.be.an("array").of.length(0);
+    }
+    {
+      const row = _readRow("|");
+      expect(row).to.be.an.instanceOf(TableRow);
+      expect(row.marginLeft).to.equal("");
+      expect(row.marginRight).to.equal("");
+      const cells = row.getCells();
+      expect(cells).to.be.an("array").of.length(0);
+    }
+    {
+      const row = _readRow("| A | B | C |");
+      expect(row).to.be.an.instanceOf(TableRow);
+      expect(row.marginLeft).to.equal("");
+      expect(row.marginRight).to.equal("");
+      const cells = row.getCells();
+      expect(cells).to.be.an("array").of.length(3);
+      expect(cells[0].rawContent).to.equal(" A ");
+      expect(cells[1].rawContent).to.equal(" B ");
+      expect(cells[2].rawContent).to.equal(" C ");
+    }
+    {
+      const row = _readRow(" | A | B | C |  ");
+      expect(row).to.be.an.instanceOf(TableRow);
+      expect(row.marginLeft).to.equal(" ");
+      expect(row.marginRight).to.equal("  ");
+      const cells = row.getCells();
+      expect(cells).to.be.an("array").of.length(3);
+      expect(cells[0].rawContent).to.equal(" A ");
+      expect(cells[1].rawContent).to.equal(" B ");
+      expect(cells[2].rawContent).to.equal(" C ");
+    }
+    {
+      const row = _readRow(" A | B | C ");
+      expect(row).to.be.an.instanceOf(TableRow);
+      expect(row.marginLeft).to.equal("");
+      expect(row.marginRight).to.equal("");
+      const cells = row.getCells();
+      expect(cells).to.be.an("array").of.length(3);
+      expect(cells[0].rawContent).to.equal(" A ");
+      expect(cells[1].rawContent).to.equal(" B ");
+      expect(cells[2].rawContent).to.equal(" C ");
+    }
+    {
+      const row = _readRow("| `\\` \\| B `|` C |");
+      expect(row).to.be.an.instanceOf(TableRow);
+      expect(row.marginLeft).to.equal("");
+      expect(row.marginRight).to.equal("");
+      const cells = row.getCells();
+      expect(cells).to.be.an("array").of.length(1);
+      expect(cells[0].rawContent).to.equal(" `\\` \\| B `|` C ");
+    }
   });
 });
