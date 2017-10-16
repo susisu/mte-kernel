18c582b86b1838a77c645635b0abb454e3623a4f
Susisu <susisu2413@yahoo.co.jp>
2017-10-16 18:31:01 +0900

    Add some interface methods

--- test/text-editor.js
+++ test/text-editor.js
@@ -1,5 +1,7 @@
 import { expect } from "chai";
 
+import { Point } from "../lib/point.js";
+import { Range } from "../lib/range.js";
 import { ITextEditor } from "../lib/text-editor.js";
 
 /**
@@ -13,6 +15,31 @@ describe("ITextEditor", () => {
     it("should be not implemented, throw an error", () => {
       const textEditor = new ITextEditor();
       expect(() => { textEditor.getCursorPos(); }).to.throw(Error, /not implemented/i);
+    });
+  });
+
+  /**
+   * @test {ITextEditor#setCursorPos}
+   */
+  describe("#setCursorPos(pos)", () => {
+    it("should be not implemented, throw an error", () => {
+      const textEditor = new ITextEditor();
+      const pos = new Point(0, 0);
+      expect(() => { textEditor.setCursorPos(pos); }).to.throw(Error, /not implemented/i);
+    });
+  });
+
+  /**
+   * @test {ITextEditor#setSelection}
+   */
+  describe("#setSelection()", () => {
+    it("should be not implemented, throw an error", () => {
+      const textEditor = new ITextEditor();
+      const range = new Range(
+        new Point(0, 0),
+        new Point(0, 1)
+      );
+      expect(() => { textEditor.setSelection(range); }).to.throw(Error, /not implemented/i);
     });
   });
 
@@ -33,6 +60,50 @@ describe("ITextEditor", () => {
     it("should be not implemented, throw an error", () => {
       const textEditor = new ITextEditor();
       expect(() => { textEditor.getLine(0); }).to.throw(Error, /not implemented/i);
+    });
+  });
+
+  /**
+   * @test {ITextEditor#insertLine}
+   */
+  describe("#insertLine()", () => {
+    it("should be not implemented, throw an error", () => {
+      const textEditor = new ITextEditor();
+      expect(() => { textEditor.insertLine(0, "foobar"); }).to.throw(Error, /not implemented/i);
+    });
+  });
+
+  /**
+   * @test {ITextEditor#deleteLine}
+   */
+  describe("#deleteLine()", () => {
+    it("should be not implemented, throw an error", () => {
+      const textEditor = new ITextEditor();
+      expect(() => { textEditor.deleteLine(0); }).to.throw(Error, /not implemented/i);
+    });
+  });
+
+  /**
+   * @test {ITextEditor#replaceLines}
+   */
+  describe("#replaceLines()", () => {
+    it("should be not implemented, throw an error", () => {
+      const textEditor = new ITextEditor();
+      const lines = [
+        "foo",
+        "bar"
+      ];
+      expect(() => { textEditor.replaceLines(0, 1, lines); }).to.throw(Error, /not implemented/i);
+    });
+  });
+
+  /**
+   * @test {ITextEditor#transact}
+   */
+  describe("#transact()", () => {
+    it("should be not implemented, throw an error", () => {
+      const textEditor = new ITextEditor();
+      expect(() => { textEditor.transact(() => {}); }).to.throw(Error, /not implemented/i);
     });
   });
 });
