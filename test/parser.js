97ee79b3a61d1e0df322511923e5bbf4b97ddfb4
Susisu <susisu2413@yahoo.co.jp>
2018-02-26 22:43:03 +0900

    Add `leftMarginChars` option to the parser

--- test/parser.js
+++ test/parser.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 
 import { TableRow } from "../lib/table-row.js";
 import { Table } from "../lib/table.js";
-import { _splitCells, _readRow, readTable } from "../lib/parser.js";
+import { _splitCells, _readRow, _marginRegex, readTable } from "../lib/parser.js";
 
 /**
  * @test {_splitCells}
@@ -103,78 +103,224 @@ describe("_readRow(text)", () => {
 });
 
 /**
+ * @test {_marginRegex}
+ */
+describe("_marginRegex(chars)", () => {
+  it("should create a regular expression object that matches whitespaces or specified characters", () => {
+    {
+      const re = _marginRegex(new Set());
+      expect(re.test("")).to.be.true;
+      expect(re.test(" \t")).to.be.true;
+      expect(re.test(" ABC ")).to.be.false;
+    }
+    {
+      const re = _marginRegex(new Set(["*", "-"]));
+      expect(re.test("")).to.be.true;
+      expect(re.test(" \t")).to.be.true;
+      expect(re.test(" ABC ")).to.be.false;
+      expect(re.test(" * ")).to.be.true;
+      expect(re.test(" -- ")).to.be.true;
+      expect(re.test(" *- ")).to.be.true;
+      expect(re.test(" A * B - C ")).to.be.false;
+    }
+    {
+      const re = _marginRegex(new Set(["\u{1F363}"]));
+      expect(re.test("")).to.be.true;
+      expect(re.test(" \t")).to.be.true;
+      expect(re.test(" ABC ")).to.be.false;
+      expect(re.test("\u{1F363}")).to.be.true;
+      expect(re.test("\uD83C")).to.be.false; // "\u{1F363}" === "\uD83C\uDF63"
+    }
+  });
+
+  it("should ignore a pipe and a backquote in the argument", () => {
+    const re = _marginRegex(new Set(["|", "`", "*"]));
+    expect(re.test("")).to.be.true;
+    expect(re.test(" \t")).to.be.true;
+    expect(re.test(" ABC ")).to.be.false;
+    expect(re.test(" * ")).to.be.true;
+    expect(re.test(" | ")).to.be.false;
+    expect(re.test(" ` ")).to.be.false;
+  });
+});
+
+/**
  * @test {readTable}
  */
-describe("readTable(lines)", () => {
+describe("readTable(lines, options)", () => {
   it("should read a table from an array of texts, each text represents a row", () => {
-    const lines = [
-      "",
-      " |  ",
-      "| A | B | C |",
-      " | A | B | C |  ",
-      " A | B | C ",
-      "| `\\` \\| B `|` C |"
-    ];
-    const table = readTable(lines);
-    expect(table).to.be.an.instanceOf(Table);
-    const rows = table.getRows();
-    expect(rows).to.be.an("array").of.length(6);
     {
-      const row = rows[0];
-      expect(row.marginLeft).to.equal("");
-      expect(row.marginRight).to.equal("");
-      const cells = row.getCells();
-      expect(cells).to.be.an("array").of.length(0);
+      const lines = [
+        "",
+        " |  ",
+        "| A | B | C |",
+        " | A | B | C |  ",
+        " A | B | C ",
+        " * | A | B | C |",
+        "| `\\` \\| B `|` C |"
+      ];
+      const options = {
+        leftMarginChars: new Set()
+      };
+      const table = readTable(lines, options);
+      expect(table).to.be.an.instanceOf(Table);
+      const rows = table.getRows();
+      expect(rows).to.be.an("array").of.length(7);
+      {
+        const row = rows[0];
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(0);
+      }
+      {
+        const row = rows[1];
+        expect(row.marginLeft).to.equal(" ");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(1);
+        expect(cells[0].rawContent).to.equal("  ");
+      }
+      {
+        const row = rows[2];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[3];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal(" ");
+        expect(row.marginRight).to.equal("  ");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[4];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[5];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(4);
+        expect(cells[0].rawContent).to.equal(" * ");
+        expect(cells[1].rawContent).to.equal(" A ");
+        expect(cells[2].rawContent).to.equal(" B ");
+        expect(cells[3].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[6];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(1);
+        expect(cells[0].rawContent).to.equal(" `\\` \\| B `|` C ");
+      }
     }
     {
-      const row = rows[1];
-      expect(row.marginLeft).to.equal(" ");
-      expect(row.marginRight).to.equal("");
-      const cells = row.getCells();
-      expect(cells).to.be.an("array").of.length(1);
-      expect(cells[0].rawContent).to.equal("  ");
-    }
-    {
-      const row = rows[2];
-      expect(row).to.be.an.instanceOf(TableRow);
-      expect(row.marginLeft).to.equal("");
-      expect(row.marginRight).to.equal("");
-      const cells = row.getCells();
-      expect(cells).to.be.an("array").of.length(3);
-      expect(cells[0].rawContent).to.equal(" A ");
-      expect(cells[1].rawContent).to.equal(" B ");
-      expect(cells[2].rawContent).to.equal(" C ");
-    }
-    {
-      const row = rows[3];
-      expect(row).to.be.an.instanceOf(TableRow);
-      expect(row.marginLeft).to.equal(" ");
-      expect(row.marginRight).to.equal("  ");
-      const cells = row.getCells();
-      expect(cells).to.be.an("array").of.length(3);
-      expect(cells[0].rawContent).to.equal(" A ");
-      expect(cells[1].rawContent).to.equal(" B ");
-      expect(cells[2].rawContent).to.equal(" C ");
-    }
-    {
-      const row = rows[4];
-      expect(row).to.be.an.instanceOf(TableRow);
-      expect(row.marginLeft).to.equal("");
-      expect(row.marginRight).to.equal("");
-      const cells = row.getCells();
-      expect(cells).to.be.an("array").of.length(3);
-      expect(cells[0].rawContent).to.equal(" A ");
-      expect(cells[1].rawContent).to.equal(" B ");
-      expect(cells[2].rawContent).to.equal(" C ");
-    }
-    {
-      const row = rows[5];
-      expect(row).to.be.an.instanceOf(TableRow);
-      expect(row.marginLeft).to.equal("");
-      expect(row.marginRight).to.equal("");
-      const cells = row.getCells();
-      expect(cells).to.be.an("array").of.length(1);
-      expect(cells[0].rawContent).to.equal(" `\\` \\| B `|` C ");
+      const lines = [
+        "",
+        " |  ",
+        "| A | B | C |",
+        " | A | B | C |  ",
+        " A | B | C ",
+        " * | A | B | C |",
+        "| `\\` \\| B `|` C |"
+      ];
+      const options = {
+        leftMarginChars: new Set("*")
+      };
+      const table = readTable(lines, options);
+      expect(table).to.be.an.instanceOf(Table);
+      const rows = table.getRows();
+      expect(rows).to.be.an("array").of.length(7);
+      {
+        const row = rows[0];
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(0);
+      }
+      {
+        const row = rows[1];
+        expect(row.marginLeft).to.equal(" ");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(1);
+        expect(cells[0].rawContent).to.equal("  ");
+      }
+      {
+        const row = rows[2];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[3];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal(" ");
+        expect(row.marginRight).to.equal("  ");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[4];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[5];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal(" * ");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(3);
+        expect(cells[0].rawContent).to.equal(" A ");
+        expect(cells[1].rawContent).to.equal(" B ");
+        expect(cells[2].rawContent).to.equal(" C ");
+      }
+      {
+        const row = rows[6];
+        expect(row).to.be.an.instanceOf(TableRow);
+        expect(row.marginLeft).to.equal("");
+        expect(row.marginRight).to.equal("");
+        const cells = row.getCells();
+        expect(cells).to.be.an("array").of.length(1);
+        expect(cells[0].rawContent).to.equal(" `\\` \\| B `|` C ");
+      }
     }
   });
 });
