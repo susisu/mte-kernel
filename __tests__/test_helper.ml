5be835a910969713696574b1253cde6c3ef4e8cb
Susisu <susisu@hatena.ne.jp>
2018-06-28 23:09:32 +0900

    Do not use magic

--- __tests__/test_helper.ml
+++ __tests__/test_helper.ml
@@ -1,5 +1,2 @@
-(* This coercion is safe since return value of the function will be ignored *)
-let _toThrowAssertionFailure = Obj.magic @@
-  Jest.Expect.toThrowMessageRe (Js.Re.fromStringWithFlags "assert" ~flags:"i")
-let toThrowAssertionFailure: [< (unit -> 'a) Jest.Expect.partial] -> Jest.assertion =
-  _toThrowAssertionFailure
+let toThrowAssertionFailure p =
+  Jest.Expect.toThrowMessageRe (Js.Re.fromStringWithFlags "assert" ~flags:"i") p
