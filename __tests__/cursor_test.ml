8752e4452780cecb22dcee14bbe360abd18bf76a
Susisu <susisu@hatena.ne.jp>
2018-06-28 22:00:32 +0900

    Improve test for Cursor module

--- __tests__/cursor_test.ml
+++ __tests__/cursor_test.ml
@@ -5,17 +5,22 @@ let () =
   describe "Cursor" begin fun () ->
     let open Cursor in
     describe "create" begin fun () ->
-      test "it should create a cursor" begin fun () ->
-        let c = create 2 4 in
-        expect (row c, column c) |> toEqual (2, 4)
+      testAll "it should create a cursor" [
+        (0, 0);
+        (0, 1);
+        (1, 0);
+        (1, 2);
+      ] begin fun (r, c) ->
+        let cursor = create r c in
+        expect (row cursor, column cursor) |> toEqual (r, c)
       end;
 
       test "it should fail if row is negative" begin fun () ->
-        expect (fun () -> create (-2) 4) |> toThrow
+        expect (fun () -> create (-1) 0) |> toThrow
       end;
 
       test "it should fail if column is negative" begin fun () ->
-        expect (fun () -> create 2 (-4)) |> toThrow
+        expect (fun () -> create 0 (-1)) |> toThrow
       end;
     end;
   end
