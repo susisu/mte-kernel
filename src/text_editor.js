9ce4d1f4ef06f261deec4a1bc9b883e991e6c5fa
Susisu <susisu@hatena.ne.jp>
2018-07-02 00:16:43 +0900

    Add AbstractTextEditor class

---
+++ src/text_editor.js
@@ -0,0 +1,45 @@
+class AbstractTextEditor {
+  getCursor() {
+    throw new Error("Not implemented: getCursorPosition");
+  }
+
+  setCursor(cursor) {
+    throw new Error("Not implemented: setCursorPosition");
+  }
+
+  setSelection(startCursor, endCursor) {
+    throw new Error("Not implemented: setSelectionRange");
+  }
+
+  getLastRow() {
+    throw new Error("Not implemented: getLastRow");
+  }
+
+  accept(row) {
+    throw new Error("Not implemented: acceptsTableEdit");
+  }
+
+  getLine(row) {
+    throw new Error("Not implemented: getLine");
+  }
+
+  insertLine(row, line) {
+    throw new Error("Not implemented: insertLine");
+  }
+
+  deleteLine(row) {
+    throw new Error("Not implemented: deleteLine");
+  }
+
+  replaceLines(range, lines) {
+    throw new Error("Not implemented: replaceLines");
+  }
+
+  transact(callback) {
+    throw new Error("Not implemented: transact");
+  }
+}
+
+module.exports = {
+  AbstractTextEditor,
+};
