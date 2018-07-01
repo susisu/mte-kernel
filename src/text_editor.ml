47165177fc01ef5f58013cabb8a2655baf8446cb
Susisu <susisu@hatena.ne.jp>
2018-07-01 23:39:39 +0900

    Add Text_editor module

---
+++ src/text_editor.ml
@@ -0,0 +1,41 @@
+type t
+
+module Js_interface = struct
+  external getCursor: t -> int * int = "" [@@bs.send]
+  external setCursor: t -> int * int -> unit = "" [@@bs.send]
+  external setSelection: t -> int * int -> int * int -> unit = "" [@@bs.send]
+  external getLastRow: t -> int = "" [@@bs.send]
+  external accept: t -> int -> bool = "" [@@bs.send]
+  external getLine: t -> int -> string = "" [@@bs.send]
+  external insertLine: t -> int -> string -> unit = "" [@@bs.send]
+  external deleteLine: t -> int -> unit = "" [@@bs.send]
+  external replaceLines: t -> int * int -> string array -> unit = "" [@@bs.send]
+  external transact: t -> (unit -> unit [@bs]) -> unit = "" [@@bs.send]
+end
+
+let getCursor t =
+  let (r, c) = Js_interface.getCursor t in
+  Cursor.create r c
+let setCursor t cursor =
+  let c = (Cursor.row cursor, Cursor.column cursor) in
+  Js_interface.setCursor t c
+let setSelection t start_cursor end_cursor =
+  let s = (Cursor.row start_cursor, Cursor.column start_cursor) in
+  let e = (Cursor.row end_cursor, Cursor.column end_cursor) in
+  Js_interface.setSelection t s e
+let getLastRow t =
+  Js_interface.getLastRow t
+let accept t row =
+  Js_interface.accept t row
+let getLine t row =
+  Js_interface.getLine t row
+let insertLine t row line =
+  Js_interface.insertLine t row line
+let deleteLine t row =
+  Js_interface.deleteLine t row
+let replaceLines t range text =
+  let r = (Range.start_row range, Range.end_row range) in
+  let lines = Array.of_list text in
+  Js_interface.replaceLines t r lines
+let transact t f =
+  Js_interface.transact t (fun [@bs] () -> f ())
---
+++ src/text_editor.mli
@@ -0,0 +1,12 @@
+type t
+
+val  getCursor: t -> Cursor.t
+val  setCursor: t -> Cursor.t -> unit
+val  setSelection: t -> Cursor.t -> Cursor.t -> unit
+val  getLastRow: t -> int
+val  accept: t -> int -> bool
+val  getLine: t -> int -> string
+val  insertLine: t -> int -> string -> unit
+val  deleteLine: t -> int -> unit
+val  replaceLines: t -> Range.t -> string list -> unit
+val  transact: t -> (unit -> unit) -> unit
