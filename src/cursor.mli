9134e66815b4a76a1209323e3b8da9ee63c627c3
Susisu <susisu@hatena.ne.jp>
2018-07-16 15:47:43 +0900

    Add doc comments for Cursor module

--- src/cursor.mli
+++ src/cursor.mli
@@ -1,6 +1,13 @@
+(** Cursor.t represents a position in the text editor. *)
 type t
 
+(** Creates a new cursor.
+    @param row Row index. Must be >= 0.
+    @param column Column index. Must be >= 0.
+*)
 val create: row:int -> column:int -> t
 
+(** Gets row index of the given cursor. *)
 val row: t -> int
+(** Gets column index of the given cursor. *)
 val column: t -> int
