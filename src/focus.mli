8def81943fe6ceeacffa714cbb792b810a5e190d
Susisu <susisu@hatena.ne.jp>
2018-07-22 15:17:26 +0900

    Add doc comments for Focus module

--- src/focus.mli
+++ src/focus.mli
@@ -1,21 +1,44 @@
+(** *)
+
 module Pointed: sig
+  (** Pointed.t represents a focus which points a specific position (offset) in a cell. *)
   type t
 
+  (** Gets the row index of the given focus. *)
   val row: t -> int
+
+  (** Gets the column index of the given focus. *)
   val column: t -> int
+
+  (** Gets the offset position in the focused cell. *)
   val offset: t -> int
 end
 
 module Selected: sig
+  (** Selected.t represents a focus which selects whole content of a cell. *)
   type t
 
+  (** Gets the row index of the given focus. *)
   val row: t -> int
+
+  (** Gets the column index of the given focus. *)
   val column: t -> int
 end
 
+(** Focus.t represents a focus on a cell. *)
 type t =
-  | Pointed of Pointed.t
-  | Selected of Selected.t
+  | Pointed of Pointed.t   (** Points a specific position (offset) in the cell. *)
+  | Selected of Selected.t (** Selects the whole content of the cell. *)
 
+(** Creates a new pointed focus.
+    @param row Row index. Negative index means the focus is in the table header.
+    @param column Column index. Must be >= 0.
+    @param offset Offset in the focused cell. Must be >= 0.
+*)
 val create_pointed: int -> int -> int -> t
+
+(** Creates a new selected focus.
+    @param row Row index. Negative index means the focus is in the table header.
+    @param column Column index. Must be >= 0.
+*)
 val create_selected: int -> int -> t
