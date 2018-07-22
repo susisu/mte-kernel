92ada5a4c9a0e62fc8f2e342b51ea4d8134f81c7
Susisu <susisu@hatena.ne.jp>
2018-07-22 18:28:19 +0900

    Add test cases for tables without header

--- __tests__/normalizer_test.ml
+++ __tests__/normalizer_test.ml
@@ -8,163 +8,306 @@ let () =
     describe "normalize" begin fun () ->
       let open Table.Normalized in
 
-      test "it should do nothing if the given table havs the normalized form" begin fun () ->
-        let table = Table.create
-            ~header:(Some ["name"; "color"])
-            ~body:[
+      describe "with header" begin fun () ->
+        test "it should do nothing if the given table havs the normalized form" begin fun () ->
+          let table = Table.create
+              ~header:(Some ["name"; "color"])
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"; "green"];
+              ]
+              ~alignments:[None; Some Alignment.Left]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            Some ["name"; "color"],
+            [
               ["apple"; "red"];
               ["banana"; "yellow"];
               ["lime"; "green"];
-            ]
-            ~alignments:[None; Some Alignment.Left]
-        in
-        let normalized = normalize table in
-        expect (
-          header normalized,
-          body normalized,
-          alignments normalized,
-          width normalized
-        ) |> toEqual (
-          Some ["name"; "color"],
-          [
-            ["apple"; "red"];
-            ["banana"; "yellow"];
-            ["lime"; "green"];
-          ],
-          [None; Some Alignment.Left],
-          2
-        )
-      end;
+            ],
+            [None; Some Alignment.Left],
+            2
+          )
+        end;
 
-      test "it should insert an header with one empty cell if there are no rows" begin fun () ->
-        let table = Table.create
-            ~header:None
-            ~body:[]
-            ~alignments:[]
-        in
-        let normalized = normalize table in
-        expect (
-          header normalized,
-          body normalized,
-          alignments normalized,
-          width normalized
-        ) |> toEqual (
-          Some [""],
-          [],
-          [None],
-          1
-        )
-      end;
+        test "it should add empty cells if there are no columns" begin fun () ->
+          let table = Table.create
+              ~header:(Some [])
+              ~body:[
+                [];
+                [];
+              ]
+              ~alignments:[]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            Some [""],
+            [
+              [""];
+              [""];
+            ],
+            [None],
+            1
+          )
+        end;
 
-      test "it should add empty cells if there are no columns" begin fun () ->
-        let table = Table.create
-            ~header:(Some [])
-            ~body:[
-              [];
-              [];
-            ]
-            ~alignments:[]
-        in
-        let normalized = normalize table in
-        expect (
-          header normalized,
-          body normalized,
-          alignments normalized,
-          width normalized
-        ) |> toEqual (
-          Some [""],
-          [
-            [""];
-            [""];
-          ],
-          [None],
-          1
-        )
-      end;
-
-      test "it should insert empty cells to rows that have less cells than the maximum one\
-           " begin fun () ->
-        let table = Table.create
-            ~header:(Some ["name"])
-            ~body:[
+        test "it should insert empty cells to rows that have less cells than the maximum one\
+             " begin fun () ->
+          let table = Table.create
+              ~header:(Some ["name"])
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"];
+              ]
+              ~alignments:[None; Some Alignment.Left]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            Some ["name"; ""],
+            [
               ["apple"; "red"];
               ["banana"; "yellow"];
-              ["lime"];
-            ]
-            ~alignments:[None; Some Alignment.Left]
-        in
-        let normalized = normalize table in
-        expect (
-          header normalized,
-          body normalized,
-          alignments normalized,
-          width normalized
-        ) |> toEqual (
-          Some ["name"; ""],
-          [
-            ["apple"; "red"];
-            ["banana"; "yellow"];
-            ["lime"; ""];
-          ],
-          [None; Some Alignment.Left],
-          2
-        )
-      end;
+              ["lime"; ""];
+            ],
+            [None; Some Alignment.Left],
+            2
+          )
+        end;
 
-      test "it should add empty alignment specifications if missing" begin fun () ->
-        let table = Table.create
-            ~header:(Some ["name"; "color"])
-            ~body:[
+        test "it should add empty alignment specifications if missing" begin fun () ->
+          let table = Table.create
+              ~header:(Some ["name"; "color"])
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"; "green"];
+              ]
+              ~alignments:[None]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            Some ["name"; "color"],
+            [
               ["apple"; "red"];
               ["banana"; "yellow"];
               ["lime"; "green"];
-            ]
-            ~alignments:[None]
-        in
-        let normalized = normalize table in
-        expect (
-          header normalized,
-          body normalized,
-          alignments normalized,
-          width normalized
-        ) |> toEqual (
-          Some ["name"; "color"],
-          [
-            ["apple"; "red"];
-            ["banana"; "yellow"];
-            ["lime"; "green"];
-          ],
-          [None; None],
-          2
-        )
-      end;
+            ],
+            [None; None],
+            2
+          )
+        end;
 
-      test "it should drop redundant alignment specifications" begin fun () ->
-        let table = Table.create
-            ~header:(Some ["name"; "color"])
-            ~body:[
+        test "it should drop redundant alignment specifications" begin fun () ->
+          let table = Table.create
+              ~header:(Some ["name"; "color"])
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"; "green"];
+              ]
+              ~alignments:[None; Some Alignment.Left; Some Alignment.Right]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            Some ["name"; "color"],
+            [
               ["apple"; "red"];
               ["banana"; "yellow"];
               ["lime"; "green"];
-            ]
-            ~alignments:[None; Some Alignment.Left; Some Alignment.Right]
-        in
-        let normalized = normalize table in
-        expect (
-          header normalized,
-          body normalized,
-          alignments normalized,
-          width normalized
-        ) |> toEqual (
-          Some ["name"; "color"],
-          [
-            ["apple"; "red"];
-            ["banana"; "yellow"];
-            ["lime"; "green"];
-          ],
-          [None; Some Alignment.Left],
-          2
-        )
+            ],
+            [None; Some Alignment.Left],
+            2
+          )
+        end;
+      end;
+
+      describe "without header" begin fun () ->
+        test "it should do nothing if the given table havs the normalized form" begin fun () ->
+          let table = Table.create
+              ~header:None
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"; "green"];
+              ]
+              ~alignments:[None; Some Alignment.Left]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            None,
+            [
+              ["apple"; "red"];
+              ["banana"; "yellow"];
+              ["lime"; "green"];
+            ],
+            [None; Some Alignment.Left],
+            2
+          )
+        end;
+
+        test "it should insert an header with one empty cell if there are no rows" begin fun () ->
+          let table = Table.create
+              ~header:None
+              ~body:[]
+              ~alignments:[]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            Some [""],
+            [],
+            [None],
+            1
+          )
+        end;
+
+        test "it should add empty cells if there are no columns" begin fun () ->
+          let table = Table.create
+              ~header:None
+              ~body:[
+                [];
+                [];
+              ]
+              ~alignments:[]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            None,
+            [
+              [""];
+              [""];
+            ],
+            [None],
+            1
+          )
+        end;
+
+        test "it should insert empty cells to rows that have less cells than the maximum one\
+             " begin fun () ->
+          let table = Table.create
+              ~header:None
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"];
+              ]
+              ~alignments:[None; Some Alignment.Left]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            None,
+            [
+              ["apple"; "red"];
+              ["banana"; "yellow"];
+              ["lime"; ""];
+            ],
+            [None; Some Alignment.Left],
+            2
+          )
+        end;
+
+        test "it should add empty alignment specifications if missing" begin fun () ->
+          let table = Table.create
+              ~header:None
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"; "green"];
+              ]
+              ~alignments:[None]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            None,
+            [
+              ["apple"; "red"];
+              ["banana"; "yellow"];
+              ["lime"; "green"];
+            ],
+            [None; None],
+            2
+          )
+        end;
+
+        test "it should drop redundant alignment specifications" begin fun () ->
+          let table = Table.create
+              ~header:None
+              ~body:[
+                ["apple"; "red"];
+                ["banana"; "yellow"];
+                ["lime"; "green"];
+              ]
+              ~alignments:[None; Some Alignment.Left; Some Alignment.Right]
+          in
+          let normalized = normalize table in
+          expect (
+            header normalized,
+            body normalized,
+            alignments normalized,
+            width normalized
+          ) |> toEqual (
+            None,
+            [
+              ["apple"; "red"];
+              ["banana"; "yellow"];
+              ["lime"; "green"];
+            ],
+            [None; Some Alignment.Left],
+            2
+          )
+        end;
       end;
     end;
   end
