68e25c48bd81bcc2e6050d76a99c32c478cb8de5
Susisu <susisu@hatena.ne.jp>
2018-08-10 09:28:36 +0900

    Include Range in Text_editor module

--- src/text_editor.ml
+++ src/text_editor.ml
@@ -25,6 +25,21 @@ module Selection = struct
   let end_cursor sel = sel.end_cursor
 end
 
+module Range = struct
+  type t = {
+    start_row: int;
+    end_row: int;
+  }
+
+  let create ~start_row ~end_row =
+    assert (start_row >= 0);
+    assert (end_row > start_row);
+    { start_row; end_row }
+
+  let start_row range = range.start_row
+  let end_row range = range.end_row
+end
+
 type t
 
 module Js_intf = struct
