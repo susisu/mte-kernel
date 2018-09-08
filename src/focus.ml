b055b0cf76a0a00841a60a7a9446ad17d496daa7
Susisu <susisu@hatena.ne.jp>
2018-09-08 19:02:16 +0900

    Simplify the definition of Focus.t

--- src/focus.ml
+++ src/focus.ml
@@ -1,28 +1,3 @@
-module Focus_point = struct
-  type t = Point.t * int
-
-  let create p o =
-    assert (p.Point.column >= 0);
-    assert (o >= 0);
-    (p, o)
-
-  let pos (p, _) = p
-  let offset (_, o) = o
-end
-
-module Focus_select = struct
-  type t = Point.t
-
-  let create p =
-    assert (p.Point.column >= 0);
-    p
-
-  let pos s = s
-end
-
 type t =
-  | Point of Focus_point.t
-  | Select of Focus_select.t
-
-let create_point ~pos ~offset = Point (Focus_point.create pos offset)
-let create_select ~pos = Select (Focus_select.create pos)
+  | Offset of Point.t * int
+  | Select of Point.t
--- src/focus.mli
+++ src/focus.mli
@@ -1,38 +1,6 @@
 (** *)
 
-module Focus_point: sig
-  (** Focus_point.t represents a focus which specifies a cell and offset position in it. *)
-  type t
-
-  (** Gets the position of the focused cell. *)
-  val pos: t -> Point.t
-
-  (** Gets the offset position in the focused cell. *)
-  val offset: t -> int
-end
-
-module Focus_select: sig
-  (** Focus_select.t represents a focus which specifies a cell and selects its content. *)
-  type t
-
-  (** Gets the position of the focused cell. *)
-  val pos: t -> Point.t
-end
-
 (** Focus.t represents a focus on a cell. *)
 type t =
-  | Point of Focus_point.t   (** Points a specific position (offset) in the focused cell. *)
-  | Select of Focus_select.t (** Selects the whole content of the focused cell. *)
-
-(** Creates a new focus specifying a offset position in the focused cell
-    @param pos Position of the focused cell in the table. Negative row index means the focus is in
-    the table header, and column index must be >= 0.
-    @param offset Offset in the focused cell. Must be >= 0.
-*)
-val create_point: pos:Point.t -> offset:int -> t
-
-(** Creates a new focus selecting the content of the focused cell.
-  @param pos Position of the focused cell in the table. Negative row index means the focus is in
-  the table header, and column index must be >= 0.
-*)
-val create_select: pos:Point.t -> t
+  | Offset of Point.t * int (* With offset in the cell. *)
+  | Select of Point.t       (* With selection of the whole content of the cell. *)
