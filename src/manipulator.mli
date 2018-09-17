ce1fd686939c33ba3071cefed7530f898e23bfcd
Susisu <susisu@hatena.ne.jp>
2018-09-17 12:46:39 +0900

    Make State.t visible from outside

--- src/manipulator.mli
+++ src/manipulator.mli
@@ -10,7 +10,15 @@ module Prim: sig
 end
 
 module State: sig
-  type t
+  type smart_cursor = {
+    start: Point.t;
+    last: Point.t;
+  }
+
+  type t = {
+    smart_cursor: smart_cursor option;
+  }
+
   val init: t
 end
 
